16fb2fda7492525f242d8664cb7e0dd5dad74e2c
Prajwal Anand <prajwalanand16@gmail.com>
2026-04-06 17:18:52 +0530

    Remove 'images/3.JPG' from moments gallery
    Removed an image from the moments gallery.

--- src/App.tsx
+++ src/App.tsx
@@ -379,7 +379,6 @@ export default function App() {
             images={[
               'images/1.JPG',
               'images/2.JPG',
-              'images/3.JPG',
               'images/4.JPG',
               'images/5.JPG',
               'images/6.JPG'
